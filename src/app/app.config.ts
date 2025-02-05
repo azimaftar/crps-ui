553dd3be13960fa5e1f22a92fa871f6f8e790462
Mustafa <mustafajalaluddin@heitech.com.my>
2025-02-05 12:38:36 +0800

    Revert last push

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,34 +1,42 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
-  withHashLocation,
-  withInMemoryScrolling,
-  withRouterConfig,
-  withViewTransitions
-} from '@angular/router';
+import { ApplicationConfig, provideZoneChangeDetection, inject, provideAppInitializer } from '@angular/core';
+import { provideRouter } from '@angular/router';
 
-import { DropdownModule, SidebarModule } from '@coreui/angular';
-import { IconSetService } from '@coreui/icons-angular';
 import { routes } from './app.routes';
+import {
+  KeycloakService,
+  provideKeycloak,
+} from 'keycloak-angular';
+import { keycloakConfig } from './keycloak-config/keycloak-config';
+
+// Keycloak initialization function
+export function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: keycloakConfig,
+      initOptions: {
+        onLoad: 'login-required', // or 'check-sso'
+        checkLoginIframe: false,
+      },
+      enableBearerInterceptor: true, // Automatically add the token to requests
+      bearerPrefix: 'Bearer',
+    });
+}
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes,
-      withRouterConfig({
-        onSameUrlNavigation: 'reload'
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideKeycloak({
+      config: keycloakConfig,
+      initOptions: {
+        onLoad: 'login-required',
+        checkLoginIframe: false,
+      },
+    }), // Provide Keycloak service with options
+    provideAppInitializer(() => {
+        const initializerFn = (initializeKeycloak)(inject(KeycloakService));
+        return initializerFn();
       }),
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'top',
-        anchorScrolling: 'enabled'
-      }),
-      withEnabledBlockingInitialNavigation(),
-      withViewTransitions(),
-      withHashLocation()
-    ),
-    importProvidersFrom(SidebarModule, DropdownModule),
-    IconSetService,
-    provideAnimationsAsync()
   ]
 };
